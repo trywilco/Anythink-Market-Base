ede5b81989ea673fc14aa7ada3c87d4182074603
Shem Magnezi <smagnezi8@gmail.com>
2021-06-02 06:36:12 +0300

    cors

--- backend/rails/config/initializers/cors.rb
+++ backend/rails/config/initializers/cors.rb
@@ -1,6 +1,6 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors, debug: true, logger: (-> { Rails.logger }) do
   allow do
-    origins 'http://localhost:3001'
+    origins '*'
 
     resource '*',
       headers: :any,
