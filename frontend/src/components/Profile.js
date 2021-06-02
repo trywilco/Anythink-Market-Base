308611bcd496e66424e91b78c431cae56a43f276
Shem Magnezi <smagnezi8@gmail.com>
2021-06-02 06:35:29 +0300

    mobile bug

--- frontend/src/components/Profile.js
+++ frontend/src/components/Profile.js
@@ -125,10 +125,10 @@ class Profile extends React.Component {
 
     return (
       <div className="profile-page">
-        <div className="user-info bg-light text-dark">
+        <div className="bg-light text-dark">
           <div className="container">
             <div className="row p-4 text-center">
-              <div className="col-xs-12 col-md-10 offset-md-1">
+              <div className="user-info col-xs-12 col-md-8 offset-md-2">
                 <img
                   src={profile.image}
                   className="user-img"
