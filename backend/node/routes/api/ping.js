3ee19496dc92301a6f0f1e30b4fb6bb9b02f315c
limormekaiten <limor.mekaiten@gmail.com>
2022-01-20 21:15:08 +0200

    feat - added ping route to our engine (node part)

---
+++ backend/node/routes/api/ping.js
@@ -0,0 +1,30 @@
+const router = require("express").Router();
+const asyncHandler = require("express-async-handler");
+const axiosLib = require("axios");
+const fs = require("fs");
+const auth = require("../auth");
+
+router.get("/",
+    auth.optional,
+    asyncHandler(async (req, res) => {
+        const baseURL = 'https://wilco-engine.herokuapp.com';
+        const axios = axiosLib.create({
+            baseURL: `${baseURL}/api/v1`,
+            headers: {
+                'Content-type': 'application/json',
+            },
+        });
+
+        try {
+            const wilcoId = fs.readFileSync('../../.wilco', 'utf8')
+
+            const result = await axios.get(`/ping/${wilcoId}`);
+
+            return res.json(result.data);
+        } catch (e) {
+            console.error(e)
+            return res.sendStatus(500);
+        }
+    }));
+
+module.exports = router;
