0c514d03cac0385776c2c35082bdea530fabfffe
limormekaiten <limor.mekaiten@gmail.com>
2022-01-27 21:06:26 +0200

    feat - rails ping controller

---
+++ backend/rails/app/controllers/ping_controller.rb
@@ -0,0 +1,23 @@
+# frozen_string_literal: true
+include Faraday
+
+class PingController < ApplicationController
+  def index
+    wilcoId = File.read("../.wilco")
+
+    puts wilcoId
+
+    conn = Faraday.new(
+      url: "https://wilco-engine.herokuapp.com/users/#{wilcoId}/",
+      headers: {'Content-Type' => 'application/json'}
+    )
+
+    response = conn.post('event') do |req|
+      req.headers['Content-Type'] = 'application/json'
+      req.body = { event: 'ping'}.to_json
+    end
+
+    render json: {}
+  end
+
+end
