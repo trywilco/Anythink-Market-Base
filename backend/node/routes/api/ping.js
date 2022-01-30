e2b699c3e214cfffb8ed56cc287ebeb0a4813114
Limor <limor.mekaiten@gmail.com>
2022-01-30 17:02:32 +0200

    feat - added ping route to our engine (node part) (#22)
    * feat - added ping route to our engine (node part)
    
    * feat - redirect to user profile after user creation
    
    * feat - rails ping controller

---
+++ backend/node/routes/api/ping.js
@@ -0,0 +1,31 @@
+const router = require("express").Router();
+const asyncHandler = require("express-async-handler");
+const axiosLib = require("axios");
+const fs = require("fs");
+const auth = require("../auth");
+
+router.get("/",
+    auth.optional,
+    asyncHandler(async (req, res) => {
+        const baseURL = 'https://wilco-engine.herokuapp.com';
+
+        const axios = axiosLib.create({
+            baseURL: baseURL,
+            headers: {
+                'Content-type': 'application/json',
+            },
+        });
+
+        try {
+            const wilcoId = fs.readFileSync('../wilco.txt', 'utf8')
+
+            const result = await axios.post(`/users/${wilcoId}/event`, JSON.stringify({ event: 'ping' }));
+
+            return res.json(result.data);
+        } catch (e) {
+            console.error(e)
+            return res.sendStatus(500);
+        }
+    }));
+
+module.exports = router;
--- backend/rails/config/routes.rb
+++ backend/rails/config/routes.rb
@@ -19,5 +19,7 @@ Rails.application.routes.draw do
     end
 
     resources :tags, only: [:index]
+
+    resources :ping, only: [:index]
   end
 end
