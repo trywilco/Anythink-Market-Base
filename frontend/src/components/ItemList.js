e65cd151bd43b865b6cc9d834a7c6adf60a2d7a8
limormekaiten <limor.mekaiten@gmail.com>
2022-02-21 12:43:59 +0200

    added classname to no items div

--- frontend/src/components/ItemList.js
+++ frontend/src/components/ItemList.js
@@ -8,7 +8,7 @@ const ItemList = (props) => {
   }
 
   if (props.items.length === 0) {
-    return <div className="py-4">No items are here... yet.</div>;
+    return <div className="py-4 no-items">No items are here... yet.</div>;
   }
 
   return (
