3126412e4d2f258846cdc791a9ca621d8f5c2988
Shem <smagnezi8@gmail.com>
2022-03-31 12:48:28 +0300

    typo

--- backend/node/app.js
+++ backend/node/app.js
@@ -27,7 +27,7 @@ app.use(express.static(__dirname + "/public"));
 
 app.use(
   session({
-    secret: "secrete",
+    secret: "secret",
     cookie: { maxAge: 60000 },
     resave: false,
     saveUninitialized: false
