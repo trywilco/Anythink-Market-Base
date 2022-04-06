d676f98891c3584af3e3bdfaffc500af2f2752dd
Lior Brauer <liorbrauer@users.noreply.github.com>
2022-04-06 13:30:21 +0300

    Update .wilco-helpers/quests_branches.js

--- .wilco-helpers/quests_branches.js
+++ .wilco-helpers/quests_branches.js
@@ -1,9 +1,10 @@
  
-// Checkpoint branches are branches that contain the state of the code that is needed to be able to start and complete a quest. 
+// Starting-point branches are branches that contain the state of the code that is needed to be able to start and complete a quest. 
 
 // There can be 2 types of checkpoint branches:
-//  1. 'checkpoint/<prevQuestName>_solution' - for quests that rely on previous quests being completed by the user in order to be played
-//  2. 'checkpoint/<questName>_prerequisite' - for quests that need a custom code setup that isn't organically added by the user in previous quests (example: a "fix-ui-bug" quest that upon starting adds a new piece of UI that wasn't present before)
+//  1. 'starting_point/<prevQuestName>_solution' - for quests that rely on previous quests being completed by the user in order to be played
+//  2. 'starting_point/<questName>_prerequisite' - for quests that need a custom code setup that isn't organically added by the user in previous quests (example: a "fix-ui-bug" quest that upon starting adds a new piece of UI that wasn't present before)
+
 
 const BRANCHES = {
   MAIN: "main",
