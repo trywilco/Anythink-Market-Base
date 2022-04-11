28dd596ae9743ada309285d8788c1bd0bd2b4115
Lior Brauer <liorbrauer@gmail.com>
2022-04-11 18:35:41 +0300

    fixes

--- .wilco-helpers/start_quest.sh
+++ .wilco-helpers/start_quest.sh
@@ -2,15 +2,16 @@
 
 echo "Stashing any existing changes..."
 git stash
-echo "Resetting local `main` branch to `origin/main`..."
+
+echo "Resetting local 'main' branch to 'origin/main'..."
 {
-  git checkout main
-  git fetch origin main
+  git checkout main &&
+  git fetch origin main &&
   git reset --hard origin/main
 } || {
-  echo "Could not reset `main` branch. Aborting."
+  echo "Could not reset 'main' branch. Aborting."
   exit 1
 }
 
-echo "Your `main` branch is now up to date."
+echo "Your 'main' branch is now up to date."
 exit 0
