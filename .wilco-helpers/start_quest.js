d1973e7b5e78b02f40252040f6fe99eb71ebe4e7
Lior Brauer <liorbrauer@gmail.com>
2022-04-11 15:16:26 +0300

    stash any existing changes

--- .wilco-helpers/start_quest.js
+++ .wilco-helpers/start_quest.js
@@ -2,6 +2,9 @@ const util = require("util");
 const exec = util.promisify(require("child_process").exec);
 
 async function reset() {
+  console.log("Stashing any existing changes...")
+  await exec("git stash")
+  console.log("Resetting local `main` brnach to origin...")
   await exec("git checkout main");
   await exec("git fetch origin main");
   await exec("git reset --hard origin/main");
