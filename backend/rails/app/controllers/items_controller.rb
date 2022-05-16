31f4faa06d99cf69f1603fb1beb3ac189fe7e72f
Dar Malovani <dar.malovani@wilcohq.com>
2022-05-16 10:54:28 +0300

    feat: performance solution

--- backend/rails/app/controllers/items_controller.rb
+++ backend/rails/app/controllers/items_controller.rb
@@ -6,7 +6,7 @@ class ItemsController < ApplicationController
   before_action :authenticate_user!, except: %i[index show]
 
   def index
-    @items = Item.includes(:tags)
+    @items = Item.includes(:tags, :user)
 
     @items = @items.tagged_with(params[:tag]) if params[:tag].present?
     @items = @items.sellered_by(params[:seller]) if params[:seller].present?
