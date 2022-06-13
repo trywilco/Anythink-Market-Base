bbc8539f4372c529a7e8e804e0bb0421bd801078
Lior Brauer <liorbrauer@users.noreply.github.com>
2022-06-13 12:04:24 +0300

    [CU-2mt7fpd] Add python (with FastAPI) backend (#93)
    * add fastpi realworld app
    
    * support running pytest in docker container
    
    * rename articles -> items
    
    * author -> seller
    
    * feat: backend now support adding item with or without image
    
    * move most setup logic to docker-compose.yml
    
    * move docker-compose out to .wilco-helpers
    
    * rename python-fastapi to python
    
    * remove unused files
    
    * speed up docker-compose for python containers
    
    * use virtual env and install poetry in Dockerfile
    
    * chore: updated readme file and cleaned realworld naming
    
    * fix:deleted redundant readme file
    
    * chore: updated readme and deleted test files
    
    * fix: returned license to its original form

---
+++ backend/python/app/api/routes/api.py
@@ -0,0 +1,16 @@
+from fastapi import APIRouter
+
+from app.api.routes import authentication, comments, profiles, tags, users
+from app.api.routes.items import api as items
+
+router = APIRouter()
+router.include_router(authentication.router, tags=["authentication"], prefix="/users")
+router.include_router(users.router, tags=["users"], prefix="/user")
+router.include_router(profiles.router, tags=["profiles"], prefix="/profiles")
+router.include_router(items.router, tags=["items"])
+router.include_router(
+    comments.router,
+    tags=["comments"],
+    prefix="/items/{slug}/comments",
+)
+router.include_router(tags.router, tags=["tags"], prefix="/tags")
