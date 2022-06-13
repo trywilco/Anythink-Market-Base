75982b7685cb01c5afadc5dd4522abf2350d1779
Lior Brauer <liorbrauer@gmail.com>
2022-06-13 16:37:12 +0300

    fix .wilco path + request creation

--- backend/python/app/services/event.py
+++ backend/python/app/services/event.py
@@ -2,7 +2,7 @@ import os
 import requests
 import json
 
-PATH_TO_WILCO_ID = '.wilco'
+PATH_TO_WILCO_ID = '../.wilco'
 BASE_URL = 'https://wilco-engine.herokuapp.com'
 WILCO_ID = os.environ.get('WILCO_ID')
 
@@ -13,11 +13,8 @@ if not WILCO_ID and os.path.exists(PATH_TO_WILCO_ID):
 EVENTS_ENDPOINT = f'{BASE_URL}/users/{WILCO_ID}/event'
 
 def send_event(event, metadata):
-    data = json.dumps(
-        {'headers': {
-            'Content-type': 'application/json',
-            'event': event,
-            **metadata
-    }})
-    res = requests.post(EVENTS_ENDPOINT, data=data)
+    headers = { 'Content-type': 'application/json' }
+    data = { 'event': event, 'metadata': metadata }
+
+    res = requests.post(EVENTS_ENDPOINT, data=json.dumps(data), headers=headers)
     return res
