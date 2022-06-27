0a808e831b7fe78699e90d8c50a361772c14db08
Tom Peri <tom.peri@wilco.work>
2022-06-27 14:21:53 +0300

    feat: consolidate error handlers + console.log

--- backend/node/app.js
+++ backend/node/app.js
@@ -67,33 +67,14 @@ app.use(function (req, res, next) {
   }
 });
 
-/// error handlers
-
-// development error handler
-// will print stacktrace
-if (!isProduction) {
-  app.use(function(err, req, res, next) {
-    console.log(err.stack);
-
-    res.status(err.status || 500);
-
-    res.json({
-      errors: {
-        message: err.message,
-        error: err
-      }
-    });
-  });
-}
-
-// production error handler
-// no stacktraces leaked to user
+/// error handler
 app.use(function(err, req, res, next) {
+  console.log(err.stack);
   res.status(err.status || 500);
   res.json({
     errors: {
       message: err.message,
-      error: {}
+      error: isProduction ? '' : err
     }
   });
 });
