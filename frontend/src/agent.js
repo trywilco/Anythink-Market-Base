5958408a4f53a2f1070df49f4bdde2964eeee2d5
Shem <shem@wilco.work>
2022-09-04 14:47:19 +0300

    Fix merge conflict

--- frontend/src/agent.js
+++ frontend/src/agent.js
@@ -6,7 +6,7 @@ const superagent = superagentPromise(_superagent, global.Promise);
 const BACKEND_URL =
   process.env.NODE_ENV !== "production"
     ? "http://localhost:3000"
-    : "https://api.anythink.market";
+    : process.env.REACT_APP_BACKEND_URL;
 
 const API_ROOT = `${BACKEND_URL}/api`;
 
