c8dba79be3d01557549a4646e9e8646377267ea7
Shem <shem@wilco.work>
2022-09-04 13:02:17 +0300

    Fix/client fixes (#138)
    * remove warnings
    
    * people always confused about how to extract the host and they sometime extract the /api too
    
    * fix yarn install warnings
    
    * remove autoprefixer for now because it introduce a new warning:
    warning Resolution field "autoprefixer@10.4.5" is incompatible with requested version "autoprefixer@^10.4.7"
    
    * change var name
    
    Co-authored-by: smagnezi8@gmail.com <shem@wilco.hq>

--- frontend/src/agent.js
+++ frontend/src/agent.js
@@ -3,10 +3,12 @@ import _superagent from "superagent";
 
 const superagent = superagentPromise(_superagent, global.Promise);
 
-const API_ROOT =
+const BACKEND_URL =
   process.env.NODE_ENV !== "production"
-    ? "http://localhost:3000/api"
-    : "https://api.anythink.market/api";
+    ? "http://localhost:3000"
+    : "https://api.anythink.market";
+
+const API_ROOT = `${BACKEND_URL}/api`;
 
 const encode = encodeURIComponent;
 const responseBody = (res) => res.body;
