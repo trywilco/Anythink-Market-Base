6459f2b25661be1137f2fc88c4be8b9ecdeeb407
Lior Brauer <liorbrauer@gmail.com>
2022-09-06 15:23:40 +0300

    cr fix

--- .devcontainer/setup.sh
+++ .devcontainer/setup.sh
@@ -1,18 +1,10 @@
 # Update engine that codespace started for user
 WILCO_ID="`cat .wilco`"
+ENGINE_EVENT_ENDPOINT="https://engine-staging.wilco.gg/users/$WILCO_ID/event"
 
-if [[ "${GITHUB_REPOSITORY}" =~ "Staging-" ]]
-  then
-    ENGINE_URL="https://engine-staging.wilco.gg/users/$WILCO_ID/event"
-  else
-    ENGINE_URL="https://engine.wilco.gg/users/$WILCO_ID/event"
-fi
+curl -L -X POST "$ENGINE_EVENT_ENDPOINT" -H "Content-Type: application/json" --data-raw "{ \"event\": \"github_codespace_started\" }"
 
-curl -L -X POST "$ENGINE_URL" -H "Content-Type: application/json" --data-raw "{
-    \"event\": \"github_codespace_started\"
-}"
-
-# Export backend url
+# Export backend url when in codespaces
 echo "export CODESPACE_BACKEND_URL=\"https://${CODESPACE_NAME}-3000.githubpreview.dev\"" >> ~/.bashrc
 
 # Change backend port visibility to public
