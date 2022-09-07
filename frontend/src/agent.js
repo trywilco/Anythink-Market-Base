3d0958014079e1490ce12aa7fce94bc9e1663a21
Lior Brauer <liorbrauer@users.noreply.github.com>
2022-09-07 10:14:10 +0300

    Basic codespaces support (#139)
    * add codespace support

--- frontend/src/agent.js
+++ frontend/src/agent.js
@@ -5,7 +5,7 @@ const superagent = superagentPromise(_superagent, global.Promise);
 
 const BACKEND_URL =
   process.env.NODE_ENV !== "production"
-    ? "http://localhost:3000"
+    ? process.env.REACT_APP_BACKEND_URL
     : "https://api.anythink.market";
 
 const API_ROOT = `${BACKEND_URL}/api`;
