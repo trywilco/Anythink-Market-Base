848714ad430dc399ca52519256d4fd2731c8481f
Lior Brauer <liorbrauer@users.noreply.github.com>
2022-09-11 11:51:30 +0300

    Fix: rails backend not working with codespaces (#149)
    * add codespace_backend_host env var for rails
    
    * use regex instead
    
    * better fix

--- .devcontainer/setup.sh
+++ .devcontainer/setup.sh
@@ -1,11 +1,14 @@
-# Update engine that codespace started for user
 WILCO_ID="`cat .wilco`"
 ENGINE_EVENT_ENDPOINT="${ENGINE_BASE_URL}/users/${WILCO_ID}/event"
+CODESPACE_BACKEND_HOST="${CODESPACE_NAME}-3000.githubpreview.dev"
+CODESPACE_BACKEND_URL="https://${CODESPACE_BACKEND_HOST}"
 
-curl -L -X POST "$ENGINE_EVENT_ENDPOINT" -H "Content-Type: application/json" --data-raw "{ \"event\": \"github_codespace_started\" }"
+# Update engine that codespace started for user
+curl -L -X POST "${ENGINE_EVENT_ENDPOINT}" -H "Content-Type: application/json" --data-raw "{ \"event\": \"github_codespace_started\" }"
 
-# Export backend url when in codespaces
-echo "export CODESPACE_BACKEND_URL=\"https://${CODESPACE_NAME}-3000.githubpreview.dev\"" >> ~/.bashrc
+# Export backend envs when in codespaces
+echo "export CODESPACE_BACKEND_HOST=\"${CODESPACE_BACKEND_HOST}\"" >> ~/.bashrc
+echo "export CODESPACE_BACKEND_URL=\"${CODESPACE_BACKEND_URL}\"" >> ~/.bashrc
 
 # Change backend port visibility to public
 echo "(&>/dev/null .devcontainer/open_port.sh &)" >> ~/.bashrc
