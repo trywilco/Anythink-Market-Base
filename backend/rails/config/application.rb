2f44cd590573c626b335de2c0c01db1a1d062e8a
Lior Brauer <liorbrauer@gmail.com>
2022-11-21 16:07:09 +0200

    move allowed hosts to application.rb

--- backend/rails/config/application.rb
+++ backend/rails/config/application.rb
@@ -6,12 +6,17 @@ require 'rails/all'
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
+CODESPACE_HOST = ENV['GITHUB_CODESPACES_PORT_FORWARDING_DOMAIN']
+
 module AnythinkMarket
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
     config.api_only = true
 
+     # Allowed hosts
+    config.hosts << ".anythink.market"
+    config.hosts << ".#{CODESPACE_HOST}" if CODESPACE_HOST.present?
 
     config.to_prepare do
       DeviseController.respond_to :html, :json
