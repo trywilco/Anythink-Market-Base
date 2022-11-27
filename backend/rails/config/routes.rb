b414fc813e9eef0966a0bbc77e1b76fc8c07bb2e
Lior Brauer <liorbrauer@gmail.com>
2022-11-27 13:48:16 +0200

    add GET /health to rails

--- backend/rails/config/routes.rb
+++ backend/rails/config/routes.rb
@@ -2,6 +2,8 @@
 Rails.application.routes.draw do
   root to: "application#root"
 
+  get '/health', to: 'application#health'
+
   scope :api, defaults: { format: :json } do
     devise_for :users, controllers: { sessions: :sessions, registrations: :registrations },
                        path_names: { sign_in: :login }
